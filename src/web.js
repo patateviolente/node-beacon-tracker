2f16974a339babee0ed29cd2233a06b2c4855399
Patrick Portal <portal@lendix.com>
2019-06-01 21:07:49 +0200

    Use logger lib

--- src/web.js
+++ src/web.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 const config = require('../config');
+const logger = require('../lib/logger');
 
 module.exports.initServer = function() {
   if (!config.dashboard.enable) {
@@ -9,10 +10,10 @@ module.exports.initServer = function() {
 
   http.createServer(routeWeb)
     .listen(config.dashboard.port);
-  console.log(`dashboard is listening on ${config.dashboard.port}`)
+  logger.log(`dashboard is listening on ${config.dashboard.port}`)
 };
 
 function routeWeb(req, res) {
-  console.log(req.url);
+  logger.log(req.url);
   res.end('Hello Node.js Server!');
 }
