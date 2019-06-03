362ef3433b5b8aeaffe765c8c04bbc94973b498f
pportal <portal@lendix.com>
2019-06-03 14:02:31 +0200

    Create TESTENV variable for tests

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const web = require('./src/web');
 const logger = require('./lib/logger');
 const config = require('./config');
 
+// TODO create configurable timeout for aggregator
+// TODO for aggregator, allow several call for a same AP, and take the better signal
+// TODO for aggregator, add configuration to whether process position whe all APP responds, or process position at custom intervals
+// TODO log when receiving aggregate call from slaves (debug)
+// TODO supervisord use node instead of npm call
+
 logger.log(`Listening on ${config.port} as ${roles.whoami} (${roles.role})`);
 
 http.createServer(router).listen(config.port);
--- src/role.js
+++ src/role.js
@@ -2,8 +2,10 @@ const config = require('../config');
 const utils = require('../lib/utils');
 
 const whoami = process.env.WHOAMI;
+const accessPoint = process.env.TESTENV
+  ? config.accessPoints[Object.keys(config.accessPoints)[0]]
+  : config.accessPoints[whoami];
 
-const accessPoint = config.accessPoints[whoami];
 if (!accessPoint) {
   utils.exit(`WHOAMI env variable is unkown (${whoami}), set value in ${Object.keys(config.accessPoints)}`);
 }
