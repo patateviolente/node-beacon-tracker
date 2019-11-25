78a3defd63ee773eea5e53e83d8f1a5be7982e30
Patrick Portal <patrick.portal@kapten.com>
2019-11-25 13:43:12 +0100

    Fix tests

--- tests/src/aggregator.approximate.js
+++ tests/src/aggregator.approximate.js
@@ -16,12 +16,20 @@ const beaconMac = utils.standardizeMac('71:bc:23:4c:72:5b');
 
 describe('aggregator - approximate', () => {
   let aggregator;
+  const accessPoints = {
+    pi1: { master: true, url: 'pimaster', x: 0.5, y: 8 },
+    pi2: { x: 7.5, y: 9 },
+    pi3: { x: 0, y: 0 },
+  };
+
   before(() => {
+    sinon.stub(config, 'accessPoints').value(accessPoints);
     Aggregator.instantiateAll();
     aggregator = Aggregator.byMAC(beaconMac);
   });
 
   beforeEach(() => {
+    sinon.stub(config, 'accessPoints').value(accessPoints);
     aggregator.addPeripheral(null);
     aggregator._responsePools = {};
   });
