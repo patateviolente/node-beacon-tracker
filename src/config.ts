68c56e35ef513d4df8b042e87e57419433ee631d
Patrick Portal <patateviolente@gmail.com>
2019-11-26 22:29:06 +0100

    Create new ZoneFinder config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import * as utils from './utils/strings';
 
 import { Bounds } from './lib/geo/RunawayBounds';
-import { AggregateConfig } from './controllers/watch/Aggregator';
+import { AggregateConfig, Pool } from './controllers/watch/Aggregator';
 
 export type BeaconConfig = {
   name: string,
@@ -76,6 +76,89 @@ export const config: any = {
     precisionCondition: pool => pool.pi2 > -95 && pool.pi3 > -95,
     runawayBounds: <Bounds>[
       [[-Infinity, -Infinity], [-1, 8]],
+    ],
+  },
+  // Optionally label a specific zone name according metrics
+  zoneFinder: {
+    enable: true,
+    // Zone side to side, will prevent impossible measure once
+    adjacent: {
+      home_tora: ['roof_tora', 'roof_caly'],
+      roof_tora: ['home_tora', 'roof_*', 'balcony_caly'],
+      roof_caly: ['home_tora', 'roof_*', 'home_caly', 'balcony_caly'],
+      roof_virigine: ['roof_*'],
+      roof_technical: ['roof_*'],
+      root_nocat_limit: ['roof_*', 'nocat_flag'],
+      nocat_flag: ['roof_*'],
+      caly_balcony: ['home_caly', 'roof_caly'],
+      home_caly: ['balcony_caly'],
+    },
+    // Zone definition
+    zones: [
+      {
+        name: 'home_tora',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 1 && pool.pi3;
+        }
+      },
+      {
+        name: 'roof_technical',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 3
+            && pool.pi1 && pool.pi2 && pool.pi3
+            && pool.pi3 < 10;
+        }
+      },
+      {
+        name: 'roof_virginie',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 2
+            && pool.pi2 && pool.pi3;
+        }
+      },
+      {
+        name: 'root_nocat_limit',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 2
+            && pool.pi1 && pool.pi2
+            && pool.pi2 > pool.pi1;
+        }
+      },
+      {
+        name: 'nocat_flag',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 1 && pool.pi2;
+        }
+      },
+      {
+        name: 'roof_tora',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length >= 2
+            && pool.pi1 && pool.pi3
+            && pool.pi3 > pool.pi1;
+        }
+      },
+      {
+        name: 'roof_caly',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length >= 2
+            && pool.pi1 && pool.pi3
+            && pool.pi3 < pool.pi1;
+        }
+      },
+      {
+        name: 'balcony_caly',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 1 && pool.pi1
+            || Object.keys(pool).length === 2 && pool.pi1 && pool.pi4;
+        }
+      },
+      {
+        name: 'home_caly',
+        context: (pool: Pool) => {
+          return Object.keys(pool).length === 1 && pool.pi4;
+        }
+      }
     ],
   },
   dashboard: {
--- src/controllers/watch/Aggregator.ts
+++ src/controllers/watch/Aggregator.ts
@@ -131,5 +131,4 @@ export default class Aggregator {
 
     return this;
   }
-
 }
